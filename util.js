66c541f22cd5a585b87217c8f397b03ba45017c4
Reynadi Andreas Priesley <43875921+Reynadi531@users.noreply.github.com>
2020-10-16 15:12:31 +0700

    try to fix bug

--- util.js
+++ util.js
@@ -4,6 +4,7 @@ const downloadCSV = (res, fileName, fields, data) => {
     const json2csv = new Parser({ fields });
     const csv = json2csv.parse(data);
     res.header('Content-Type', 'text/csv');
+    res.attachment(fileName);
     return res.send(csv);
 }
 
