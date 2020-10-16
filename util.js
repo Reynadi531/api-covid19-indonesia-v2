319105b0e744f1d911d3384c774a677b4487c7e3
Reynadi Andreas Priesley <43875921+Reynadi531@users.noreply.github.com>
2020-10-16 15:42:24 +0700

    delete attachment

--- util.js
+++ util.js
@@ -4,7 +4,6 @@ const downloadCSV = (res, fileName, fields, data) => {
     const json2csv = new Parser({ fields });
     const csv = json2csv.parse(data);
     res.header('Content-Type', 'text/csv');
-    res.attachment(fileName);
     return res.send(csv.toString());
 }
 
