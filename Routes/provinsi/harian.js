0f634b07c9860e4afb47be07209744c8355342df
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-07-31 22:00:34 +0700

    Added caching to reduce latency and data refresh function

--- Routes/provinsi/harian.js
+++ Routes/provinsi/harian.js
@@ -1,7 +1,23 @@
-const { periodicalProvData } = require('../../util/fetcher')
-module.exports = async(req, res) => {
+const NodeCache= require("node-cache")
+const axios = require('axios')
+const myCache = new NodeCache()
 
-  var data = await periodicalProvData()
+const fetch = async() =>{
+  const { data } = await axios.get("https://data.covid19.go.id/public/api/prov_time.json")
+  myCache.set("dailyProvData", data)
+  console.log("Finished initial data fetch")
+
+}
+fetch()
+setInterval(fetch, 60*60*1000)
+
+module.exports = async(req, res) => {
+  var data = await myCache.get("dailyProvData")
+  if(!data){
+    console.log("data cache not found. refreshing")
+    await fetch()
+    data = await myCache.get("dailyProvData")
+  }
   data = data.list
   const date = req.query.date || undefined
   const month = req.query.month || undefined
@@ -22,7 +38,7 @@ module.exports = async(req, res) => {
   await filterFunction().then(()=>{
     console.log(data)
     return res.json({
-      ...data
+      data
     })
   })
 }
--- util/fetcher.js
+++ util/fetcher.js
@@ -16,11 +16,6 @@ const provData = async() => {
     return data
 }
 
-const periodicalProvData = async() => {
-    const { data } = await axios.get("https://data.covid19.go.id/public/api/prov_time.json")
-    return data
-}
-
 const provDataAlt = async() => {
     const { data } = await axios.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json');
     return data.features
@@ -30,6 +25,5 @@ module.exports = {
     updateData,
     provData,
     provDataAlt,
-    periodicalProvData
     
 }
