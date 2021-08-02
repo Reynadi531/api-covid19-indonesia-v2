24206bf5fe2fabd17d13dd5bfa78c4c6687c4a7b
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-08-02 21:39:01 +0700

    Changed caching function

--- Routes/provinsi/harian.js
+++ Routes/provinsi/harian.js
@@ -1,10 +1,11 @@
 const NodeCache= require("node-cache")
 const axios = require('axios')
 const myCache = new NodeCache()
-
+const fs = require('fs')
 const fetch = async() =>{
   const { data } = await axios.get("https://data.covid19.go.id/public/api/prov_time.json")
-  myCache.set("dailyProvData", data)
+  // myCache.set("dailyProvData", data)
+  fs.writeFileSync("./harian.json", JSON.stringify(data))
   console.log("Finished initial data fetch")
 
 }
@@ -12,11 +13,13 @@ fetch()
 setInterval(fetch, 60*60*1000)
 
 module.exports = async(req, res) => {
-  var data = await myCache.get("dailyProvData")
+  // var data = await myCache.get("dailyProvData")
+  var data = await JSON.parse(fs.readFileSync("./harian.json"))
   if(!data){
     console.log("data cache not found. refreshing")
-    await fetch()
-    data = await myCache.get("dailyProvData")
+    // await fetch()
+    // data = await myCache.get("dailyProvData")
+    data = await await JSON.parse(fs.readFileSync("./harian.json"))
   }
   data = data.list
   const date = req.query.date
